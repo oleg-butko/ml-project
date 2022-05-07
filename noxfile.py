ca1ade5afe872869474a2a5c029bd2828c95915d
oleg-butko <oleg.butko@gmail.com>
2022-05-07 09:25:32 +0300

    Add nox session for tests

--- noxfile.py
+++ noxfile.py
@@ -7,8 +7,9 @@ import nox
 from nox.sessions import Session
 
 
-nox.options.sessions = "black", "mypy"
-locations = "src", "noxfile.py"
+# nox.options.sessions = "black", "mypy"
+nox.options.sessions = ["tests"]
+locations = "forest_cover_type", "noxfile.py"
 
 
 def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
@@ -16,7 +17,7 @@ def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> Non
     By default newest versions of packages are installed,
     but we use versions from poetry.lock instead to guarantee reproducibility of sessions.
     """
-    with tempfile.NamedTemporaryFile() as requirements:
+    with tempfile.NamedTemporaryFile(delete=False) as requirements:
         session.run(
             "poetry",
             "export",
@@ -29,7 +30,7 @@ def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> Non
         session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
-@nox.session(python="3.9")
+@nox.session(python="3.10")
 def black(session: Session) -> None:
     """Run black code formatter."""
     args = session.posargs or locations
@@ -37,9 +38,18 @@ def black(session: Session) -> None:
     session.run("black", *args)
 
 
-@nox.session(python="3.9")
+@nox.session(python="3.10")
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or locations
     install_with_constraints(session, "mypy")
     session.run("mypy", *args)
+
+
+@nox.session(python="3.10")
+def tests(session: Session) -> None:
+    """Run the test suite."""
+    args = session.posargs
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "pytest")
+    session.run("pytest", *args)
